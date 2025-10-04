3b2c3b4d3d62bb4d13258dd54d93648a2d6a4893
tryk <mortentryk@gmail.com>
2025-10-04 10:17:44 +0200

    3

---
+++ .next/server/app/api/tts/route_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/tts/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Users/christianhjorth/tts/":[],"/Users/christianhjorth/tts/app/api/tts/route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
